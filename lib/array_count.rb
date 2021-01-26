a41476639c8b8b88b9767adbf27e5f6738367076
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-26 23:52:10 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,14 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-array.count(String)
+  index = 0
+  number_of_strings = 0
+  while index < array.count do
+    if array[index].class == String
+      number_of_strings += 1
+    end
+    index += 1
+  end
+  number_of_strings
 end
 
 
