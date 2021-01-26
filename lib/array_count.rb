f413dfb796460d186a27ea71e363fb5721525d79
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-26 23:50:23 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,26 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  index = 0
+  number_of_strings = 0
+  while index < array.count do
+    if array[index].class == String
+      number_of_strings += 1
+    end
+    index += 1
+  end
+  number_of_strings
 end
+
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  index = 0
+count_empty_strings = 0
+  while index < array.count do
+    if array[index].class == String
+      count_empty_strings += 1
+    end
+    index += 1
+  end
+  count_empty_strings
 end
